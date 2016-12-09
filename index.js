ca6dd8c29f50fe5972a500db8ef64e9fea85f3c9
tobelum <tohbelle@gmail.com>
2016-12-09 16:52:10 +0000

    added the phone things

--- index.js
+++ index.js
@@ -469,3 +469,36 @@ function directoryATM(results)
   document.getElementById('list').innerHTML = value;
 
 }
+
+
+
+document.addEventListener("deviceready", onDeviceReady, false);
+function onDeviceReady() {
+console.log(navigator.contacts);
+
+
+document.getElementById("viewContacts").onclick = function() {
+navigator.contacts.pickContact(function(contact){
+        console.log('The following contact has been selected:' + JSON.stringify(contact));
+    },function(err){
+        console.log('Error: ' + err);
+    })
+}
+}
+
+function onSuccess(contacts) {
+    alert('Found ' + contacts.length + ' contacts.');
+};
+
+function onError(contactError) {
+    alert('onError!');
+};
+
+// find all contacts with 'Bob' in any name field
+var options      = new ContactFindOptions();
+options.filter   = "Bob";
+options.multiple = true;
+options.desiredFields = [navigator.contacts.fieldType.id];
+options.hasPhoneNumber = true;
+var fields       = [navigator.contacts.fieldType.displayName, navigator.contacts.fieldType.name];
+navigator.contacts.find(fields, onSuccess, onError, options);
